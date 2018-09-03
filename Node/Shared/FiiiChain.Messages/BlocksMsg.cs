41638aa58cf96be27660b50235d28652af0d9f5a
FiiiDeveloper <developer@fiii.io>
2018-09-03 12:14:13 +0800

    Clean up copyright info

--- Node/Shared/FiiiChain.Messages/BlocksMsg.cs
+++ Node/Shared/FiiiChain.Messages/BlocksMsg.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) 2018 FiiiLab Technology Ltd
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or or http://www.opensource.org/licenses/mit-license.php.
-// Copyright (c) 2018 FiiiLab Technology Ltd
-// Distributed under the MIT software license, see the accompanying
-// file LICENSE or or http://www.opensource.org/licenses/mit-license.php.using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
